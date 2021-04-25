df0070ffc8aa9d93b6a967ba50866933316a873a
Michal Paukszto <michal.paukszto@gmail.com>
2021-04-25 15:11:45 +0200

    samll remarks

--- src/main/java/com/sda/weather/LocationController.java
+++ src/main/java/com/sda/weather/LocationController.java
@@ -2,14 +2,8 @@ package com.sda.weather;
 
 public class LocationController {
 
-    public String addNewLocation (String cityname, String region, String countryname) {
-
-        Location location = new Location(cityname, region, countryname);
+    public String addNewLocation (String cityname, String region, String countryname) { // todo longitude and latitude are missing
+        Location location = new Location(cityname, region, countryname); // todo just pass this data to a service layer, this object is unnecessary
         return null;
     }
-
-
-
-
-
 }
--- src/main/java/com/sda/weather/LocationService.java
+++ src/main/java/com/sda/weather/LocationService.java
@@ -8,24 +8,24 @@ public class LocationService {
         this.locationRepository = locationRepository;
     }
 
-    public Location createNewLocation(String cityname, String region, String countryname){
-        if(cityname == null){
+    public Location createNewLocation(String cityname, String region, String countryname) {
+        if (cityname == null) {
             throw new RuntimeException("City name is required");
         }
-        if(countryname == null){
+        if (countryname == null) {
             throw new RuntimeException("Country name is required");
         }
 
-        if(region == null) {
-            Location location = new Location (cityname, countryname);
+        if (region == null) {
+            Location location = new Location(cityname, countryname);    // todo always call this constructor
             Location savedLocation = locationRepository.save(location);
             return savedLocation;
         }
+
+        // todo if region is not null then use location.setRegion(region)
 
         Location location = new Location(cityname, region, countryname);
         Location savedLocation = locationRepository.save(location);
         return savedLocation;
     }
-
-
 }

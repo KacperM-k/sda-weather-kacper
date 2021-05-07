4deb4079874888dcaa77f739933bfa02c45bc03e
Kacper <kacpermielnikit@gmail.com>
2021-05-07 21:28:26 +0200

    info about weather - print

--- src/main/java/com/sda/weather/UserInterface.java
+++ src/main/java/com/sda/weather/UserInterface.java
@@ -95,7 +95,6 @@ public class UserInterface {
         System.out.print("Type city name: \n");
         String cityname = scan.nextLine();
 
-        System.out.println("Information about weather: \n");
         locationController.showInfoAboutWeather(id, cityname);
         System.out.println();
 

aa8d5d209918e08e579a14c044c803b7d072ff1c
Kacper <kacpermielnikit@gmail.com>
2021-05-08 12:15:53 +0200

    add Weather class

--- src/main/java/com/sda/weather/Weather.java
+++ src/main/java/com/sda/weather/Weather.java
@@ -1,4 +1,36 @@
 package com.sda.weather;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Weather {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    private Double temperature;
+    private Double pressure;
+    private Double humidity;
+    private Double wind_speed;
+    private Double wind_degree;
+    private Instant createDate;
+    @ManyToOne
+    Location location;
+
+    public Weather(Double temperature, Double pressure, Double humidity, Double wind_speed, Double wind_degree, Instant createDate) {
+        this.temperature = temperature;
+        this.pressure = pressure;
+        this.humidity = humidity;
+        this.wind_speed = wind_speed;
+        this.wind_degree = wind_degree;
+        this.createDate = createDate;
+    }
 }
